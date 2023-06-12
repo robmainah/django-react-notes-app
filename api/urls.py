bd971a79582e0e187ad7d04a5bf2e24b9ff0b3ac
robmainah <44176348+robmainah@users.noreply.github.com>
2023-06-12 18:56:05 +0300

    get notes and notes

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.getRoutes, name='routes')
+    path('', views.getNotes, name='notes'),
+    path('notes/<int:pk>', views.getNote, name='note'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-def getRoutes(request):
-    return JsonResponse('Our Api', safe=False)
+from .models import Note
+from .serializers import NoteSerializer
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(pk=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
