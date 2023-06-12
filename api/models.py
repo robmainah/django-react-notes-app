17a7847dbd4664fbe962bfeb903d224bc6ea8d30
robmainah <44176348+robmainah@users.noreply.github.com>
2023-06-12 18:35:23 +0300

    remove cached folders

--- api/models.py
+++ api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Note(models.Model):
     body = models.TextField(null=True, blank=True)
-    updated = models.DateTimeField(auto_now=False)
-    created = models.DateTimeField(auto_now_add=False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.body[:50]
